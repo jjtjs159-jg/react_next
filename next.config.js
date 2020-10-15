ff0bd130a3ec8b49da5d2477d4996fd312ab1850
jg <jjtjs159@gmail.com>
2020-10-15 15:13:50 +0900

    정적 최적화 표시기

--- next.config.js
+++ next.config.js
@@ -11,4 +11,21 @@ module.exports = {
             }
         }),
     ],
+    devIndicators: {
+        // 페이지 우측 하단에 prerender 표시
+        autoPrerender: true,
+    }
+
+    // /**
+    //  * 리다이렉트 하는 경우, 클라이언트 라우트에서는 영향을 주지 않는다.
+    //  */
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/sad',
+    //             destination: '/teacher',
+    //             permanent: false,
+    //         },
+    //     ]
+    // },
 }
