705d86e8e440f4075b94c980a7ec8978020c455b
jg <jjtjs159@gmail.com>
2020-10-16 14:31:11 +0900

    next가 제공해주는 dotenv로 변경

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
-const dotenv = require('dotenv');
 const webpack = require('webpack');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
-dotenv.config();
-
 module.exports = withBundleAnalyzer({
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                "API_URL": JSON.stringify(process.env.API_URL),
-            }
-        }),
-    ],
+    // plugins: [
+    //     new webpack.DefinePlugin({
+    //         "process.env": {
+    //             "API_URL": JSON.stringify(process.env.API_URL),
+    //         }
+    //     }),
+    // ],
     devIndicators: {
         // 페이지 우측 하단에 prerender 표시
         autoPrerender: true,
