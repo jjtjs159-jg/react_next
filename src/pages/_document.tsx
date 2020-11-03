ae6f3cc8f8ada7934e2ee154be0709552650fc70
jg <jjtjs159@gmail.com>
2020-11-03 17:55:14 +0900

    material ui ssr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Fragment } from 'react';
+import { GetServerSideProps, NextPageContext } from 'next';
+import { ServerStyleSheets } from '@material-ui/styles';
 
 /**
  * html, body 태그 보강
@@ -25,5 +28,25 @@ class MyDocument extends Document {
         );
     }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+    console.log('get============');
+
+    ctx.renderPage = () =>
+        originalRenderPage({
+            enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+        });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+        ...initialProps,
+        // Styles fragment is rendered after the app and page rendering finish.
+        styles: <Fragment>{sheets.getStyleElement()}</Fragment>,
+    };
+};
 
 export default MyDocument;
