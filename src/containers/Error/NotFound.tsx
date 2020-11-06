5fd1e00a25db14c88332b4f225048bee24a82c2a
jg <jjtjs159@gmail.com>
2020-11-07 01:36:11 +0900

    404 css

--- src/containers/Error/NotFound.tsx
+++ src/containers/Error/NotFound.tsx
@@ -29,11 +29,13 @@ const Index: FunctionComponent = () => {
                 </h1>
             </div>
             <div>
-                <p>죄송합니다. 현재 찾을수 없는 페이지를 요청하셨습니다.</p>
-                <p>존재하지 않는 주소를 입력하셨거나</p>
-                <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
-                <p>또는, 현재 준비 중인 페이지 입니다.</p>
-                <p>감사합니다.</p>
+                <p className={styles.paragraph}>
+                    죄송합니다. <br />
+                    현재 찾을수 없는 페이지를 요청하셨습니다. <br /> 존재하지 않는 주소를
+                    입력하셨거나 요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다. <br />
+                    또는, 현재 준비 중인 페이지 입니다. <br />
+                    감사합니다.
+                </p>
             </div>
             <div className={styles.button}>
                 <RNButton type="button" href="/">
